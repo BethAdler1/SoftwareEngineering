785621ac95480ae26732da4566be685ee4b5d978
BethAdler1 <badler500@gmail.com>
2026-09-08 15:29:09 -0400

    added intentiall broken java code to test that code will fail

---
+++ src/project/annotations/Test.java
@@ -0,0 +1,8 @@
+package project.annotations;
+
+public class test { // checkstyle falure: class name not capitalized
+    public static void main(String[] args) {
+        System.out.println("Hello World") // Gradle failure due to no semicolon
+    }
+    
+}
