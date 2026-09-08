e49e557a7570f51ea4b5b5778331fd1d67825258
BethAdler1 <badler500@gmail.com>
2026-09-08 15:37:01 -0400

    change back

--- src/project/annotations/Test.java
+++ src/project/annotations/Test.java
@@ -1,8 +1,8 @@
 package project.annotations;
 
-public class Test { 
+public class test { 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World")
     }
     
 }
